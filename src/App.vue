cf2d708ce88ecad588d06c006d166ba015e88c0c
shamseldeen137 <shams.eldeen.ali@outlook.com>
2025-10-04 11:51:21 +0300

    portal

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,22 @@
 <template>
-  <!-- <Card >
-Card Content
-  </Card>
-  <Card>
-<h2> Card Content</h2>
-  </Card>
-
-
-<Card>
-<template v-slot:card-header>
-  <h3> This is Card Header</h3>
-</template>
-<template v-slot:default>
-  <img src="https://picsum.photos/200"/>
-</template>
-<template v-slot:card-footer>
-  <h4> This is Card Footer</h4>
-  <button> View Details</button>
-</template>
-</Card> -->
-
-<!-- <NameList >
-  <template v-slot:default="slotProps">
-    <span style="color: red"> {{ slotProps.fristName }}</span>
-
-  </template>
-</NameList>
-<NameList >
-  <template v-slot:default="slotProps">
-    <span style="color: red"> {{ slotProps.fristName }} {{ slotProps.lastName }}</span>
-
-  </template>
-</NameList> -->
-
-
-<!-- <h4>
-  This is App Component
-</h4>
-<ChildStyle >
-  
-
-  <h4>
-    This is Child Style Component
-</h4>
-  
-  
-  </ChildStyle> -->
-<button @click="activTab='TabA'"> Tab A</button>
-<button @click="activTab='TabB'"> Tab B</button>
-<button @click="activTab='TabC'"> Tab C</button>
-<!-- 
-<TabA  v-if="activTab==='TabA'"/>
-<TabB  v-if="activTab==='TabB'"  />
-<TabC  v-if="activTab==='TabC'"/> -->
-<KeepAlive >
-<component :is="activTab"/>
-</KeepAlive>
+  <Teleport to="#portal-root">
+  <portal/>
+  </Teleport>
 
 </template>
 
 <script>
-//import Card from './components/Card.vue';
-//import NameList from './components/NameList.vue';z
-//import ChildStyle from './components/ChildStyle.vue';
-import TabA from './components/TabA.vue';
-import TabB from './components/TabB.vue';
-import TabC from './components/TabC.vue';
+import Portal from './components/Portal.vue';
 export default {
   name: 'App',
   components: {
-    // Greet,
-    //ComponentC,
-    //Card,
-   // NameList
-    //ChildStyle,
-    TabA,
-    TabB,
-    TabC
+    Portal
+   
 
     
   },
   data() {
     return {
-    activTab:'TabA'
       
     }
   },
@@ -100,7 +34,5 @@ export default {
   margin-top: 60px;
 
 }
-h4 {
-  color: orange;
-}
+
 </style>
---
+++ src/components/Portal.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2> this Is Portal    </h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "PortalComponent"
+    }
+</script>
+
+<style scoped>
+
+</style>
